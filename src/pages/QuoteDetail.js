c6dfc9d32039fd5ac10c5e3c28d9ce9d2d54f469
Eduard Ciugulea <ciugulea@yahoo.com>
2021-09-01 18:43:08 +0100

    connect comments with a database

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,29 +1,54 @@
 import { useParams, Route, Link, useRouteMatch } from "react-router-dom";
+import { useEffect } from "react";
 
-import Comments from "../components/comments/Comments";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
+import Comments from "../components/comments/Comments";
+import useHttp from "../hooks/use-http";
+import { getSingleQuote } from "../lib/api";
+import LoadingSpinner from "../components/UI/LoadingSpinner";
 
-const DUMMY_QUOTES = [
-  { id: "q1", author: "Edi", text: "Learning React is fun!" },
-  { id: "q2", author: "Eduard", text: "Learning Coding is great!" },
-];
 
 const QuoteDetail = () => {
   const match = useRouteMatch();
   const params = useParams();
-  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+  const { quoteId } = params;
+  const {
+    sendRequest,
+    status,
+    data: loadedQuote,
+    error,
+  } = useHttp(getSingleQuote, true);
 
-  if (!quote) {
+  useEffect(() => {
+    sendRequest(quoteId);
+
+  }, [sendRequest, quoteId]);
+
+  if (status === 'pending') {
+    return <div className='centered'>
+      <LoadingSpinner />
+    </div>
+  }
+
+  if (error) {
+    return <p className="centered focused">
+      {error}
+    </p>
+  }
+
+  if (!loadedQuote.text) {
     return <p> No Quote Found </p>;
   }
 
   return (
     <>
-      <HighlightedQuote text={quote.text} author={quote.author} />
+      <HighlightedQuote text={loadedQuote.text} author={loadedQuote.author} />
       <Route path={match.path} exact>
-      <div className="centered">
-        <Link className="btn--flat" to={`${match.url}}/comments`}> Load Comments </Link>
-      </div>
+        <div className="centered">
+          <Link className="btn--flat" to={`${match.url}/comments`}>
+            Load Comments
+          </Link>
+        </div>
       </Route>
       <Route path={`${match.path}/comments`}>
         <Comments />
