e005d410aced2299b93fdadee7c3134d6b26a7fb
Eduard Ciugulea <ciugulea@yahoo.com>
2021-09-01 17:41:59 +0100

    application connected to database

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,19 +1,35 @@
-import { useParams, Route } from 'react-router-dom';
+import { useParams, Route, Link, useRouteMatch } from "react-router-dom";
 
-import Comments from '../components/comments/Comments'
+import Comments from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Edi", text: "Learning React is fun!" },
+  { id: "q2", author: "Eduard", text: "Learning Coding is great!" },
+];
 
 const QuoteDetail = () => {
-
+  const match = useRouteMatch();
   const params = useParams();
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
 
-  return <>
-    <h1>Quote {params.quoteId} Detail Page</h1>
-    <Route path={`/quotes/${params.quoteId}/comments`}>
-      <Comments />
-    </Route>
-  
-  </>
-  
+  if (!quote) {
+    return <p> No Quote Found </p>;
+  }
+
+  return (
+    <>
+      <HighlightedQuote text={quote.text} author={quote.author} />
+      <Route path={match.path} exact>
+      <div className="centered">
+        <Link className="btn--flat" to={`${match.url}}/comments`}> Load Comments </Link>
+      </div>
+      </Route>
+      <Route path={`${match.path}/comments`}>
+        <Comments />
+      </Route>
+    </>
+  );
 };
 
 export default QuoteDetail;
